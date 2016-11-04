94f381623840763560cba8b2a1266dbeeb9ab77b
Brandon Mork <brandon.mork@yahoo.com>
2016-11-04 15:24:19 -0500

    Add files via upload

--- myFunctions.h
+++ myFunctions.h
@@ -13,29 +13,47 @@ string gender();
 
 class profile
 {
-private:
-  string user;
-  string pass;
-  string outColor;
-  string eyeColor;
-  string hairColor;
-  string skinColor;
-  string gender;
+  private:
+    string user;
+    string pass;
+    string outColor;
+    string eyeColor;
+    string hairColor;
+    string skinColor;
+    string gender;
 
-public:
-  profile(string, string, string, string, string, string, string);
-  string getHair() const;
-  string getSkin() const;
-  string getEye() const;
-  string getOutfit() const;
-  string getGender() const;
-  void setPassword();
-  void setHair();
-  void setSkin();
-  void setEye();
-  void setOutfit();
-  void setGender();
+  public:
+    profile(string, string, string, string, string, string, string);
+    string getHair() const;
+    string getSkin() const;
+    string getEye() const;
+    string getOutfit() const;
+    string getGender() const;
+    void setPassword();
+    void setHair();
+    void setSkin();
+    void setEye();
+    void setOutfit();
+    void setGender();
+};
 
+class pokemon
+{
+  private:
+    string type;
+    string name;
+    double cp;
+    double hp;
+
+  public:
+    string getType()const;
+    string getName()const;
+    double getCp()const;
+    double getHp()const;
+    void setType();
+    void setName();
+    void setCp();
+    void setHp();
 };
 
 #endif // MYFUNCTIONS_H
